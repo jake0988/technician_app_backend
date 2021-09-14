66153e8f09407b4f945ea9010eddcc24a5adeb0c
Jake <jake098877@gmail.com>
2021-09-14 16:58:58 -0400

    Adds current_user and logged_in? methods.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,34 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    byebug
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      # byebug
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
-  def show
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
+  end
 
+  def destroy
+    session.clear
+    render json: {
+      notice: "Successfully logged out"
+      
+    },
+    status: :ok
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
-
+include ::ActionController::Cookies
     def current_user
-      User.first
+      User.find_by(id: session[:user_id])
     end
 
-    def logged_in
+    def logged_in?
       !!current_user
     end
 end
