615fcd950c75f15e1aa4b14ac1358a3710c1e3df
Jake <jake098877@gmail.com>
2021-09-15 19:21:23 -0400

    Adds Customer methods.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+    render json CustomerSerializer.new(@customers)
+  end
+  
+  def create
+    @customer = Customer.new
+    if @customer.save
+
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
