a5e853a0d2f6c7cccb6d1be120daef92893404bf
Jake <jake098877@gmail.com>
2021-09-15 20:21:02 -0400

    Adds some seed data.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::CustomersController < ApplicationController
   def index
     @customers = Customer.all
-    render json CustomerSerializer.new(@customers)
+    render json: CustomerSerializer.new(@customers)
   end
   
   def create
     @customer = Customer.new
     if @customer.save
+    end
 
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 User.create(name: "Bobby", username: "BobbyUser", password: "password")
 Customer.create(name: "Sally Jessie", address: "1515 Park Ave NY, NY 10001, phone_number: 343 3433444", number_of_pianos: 1, user_id: 1)
 Piano.create(make: "Steinway", model: "M", serial: "M1242", user_id: 1, customer_id: 1, year: 1942, future_work: "Repair soundboard, voicing, ivory chip repair, repair casters, fix lyre, check dip, strike, aftertouch, clean up key-height.")
@@ -12,3 +13,20 @@ Appointment.create(initial_a4: 436, work_done: "Tuning, voiced high treble.", pr
 p = Piano.first
 a = Appointment.first
 p.appointments << a
+
+
+a = Customer.create(name: "Sam John",  email: "sjohn@gmail.com", user_id: 1,address: "123-abc st.", phone_number: "(123) 123-1234")
+b = Customer.create(name: "Erving Smith",  email: "esmith@gmail.com", user_id: 1, address: "123-abc st.", phone_number: "(123) 123-1234")
+c = Customer.create(name:"Hans Gettleman",  email: "hgettleman@gmail.com", user_id: 1, address: "123-abc st.", phone_number: "(123) 123-1234")
+# byebug
+d = Piano.new(make: "Steinway", model: "B", serial: "09124WFEW", year: 2000, image: "https://steinwaypianodb.imgix.net/584537/ABubinga1.jpg?&w=1080&h=auto", future_work: "YIKES!")
+
+e = Piano.new(make: "Boston", serial: "lkjans42", year: 2003, image: "https://cdn.shopify.com/s/files/1/0050/7160/1782/products/IMG_3024-1_compact.jpg?v=1614120053", future_work: "uggh, craigslist")
+
+f = Piano.new(make: "Mason & Hamlin", model: "BB", serial: "lkSFns42", year: 1949, image: "https://www.jimlaabsmusicstore.com/wp-content/uploads/2019/06/Mason-Hamlin-BB.jpeg", future_work: "surpisingly musical")
+
+g = Piano.new(make: "Boston", serial: "lkjans42", year: 1993, image: "https://i.ebayimg.com/images/g/kAsAAOSwKtlWq~YX/s-l1600.jpg", future_work: "none too shabby!")
+a.pianos << d
+b.pianos << e
+c.pianos << f
+a.pianos << g
