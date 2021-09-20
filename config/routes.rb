37027ec44681736aca7b32b65ac70e0f44142247
Jake <jake098877@gmail.com>
2021-09-20 16:33:24 -0400

    Adds signup route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
   delete "/api/v1/delete", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  
+  post "/api/v1/signup", to: "api/v1/users#create" 
+
   namespace :api do
     namespace :v1 do
       resources :users    
