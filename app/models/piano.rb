0656ad1ac434a3f4ae47cba9b6136ebb10c2a11d
Jake <jake098877@gmail.com>
2021-10-17 19:22:09 -0400

    Adds active storage.

--- app/models/piano.rb
+++ app/models/piano.rb
@@ -2,4 +2,5 @@ class Piano < ApplicationRecord
   has_many :appointment_pianos
   has_many :appointments, through: :appointment_pianos
   belongs_to :customer
+  has_many_attached :images
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,13 @@ a = Customer.create(name: "Sam John",  email: "sjohn@gmail.com", user_id: 1,addr
 b = Customer.create(name: "Erving Smith",  email: "esmith@gmail.com", user_id: 1, address: "123-abc st.", phone_number: "(123) 123-1234")
 c = Customer.create(name:"Hans Gettleman",  email: "hgettleman@gmail.com", user_id: 1, address: "123-abc st.", phone_number: "(123) 123-1234")
 # byebug
-d = Piano.new(make: "Steinway", model: "B", serial: "09124WFEW", year: 2000, image: "https://steinwaypianodb.imgix.net/584537/ABubinga1.jpg?&w=1080&h=auto", notes: "YIKES!")
+d = Piano.new(make: "Steinway", model: "B", serial: "09124WFEW", year: 2000, images: "https://steinwaypianodb.imgix.net/584537/ABubinga1.jpg?&w=1080&h=auto", notes: "YIKES!")
 
-e = Piano.new(make: "Boston", serial: "lkjans42", year: 2003, image: "https://cdn.shopify.com/s/files/1/0050/7160/1782/products/IMG_3024-1_compact.jpg?v=1614120053", notes: "uggh, craigslist")
+e = Piano.new(make: "Boston", serial: "lkjans42", year: 2003, images: "https://cdn.shopify.com/s/files/1/0050/7160/1782/products/IMG_3024-1_compact.jpg?v=1614120053", notes: "uggh, craigslist")
 
-f = Piano.new(make: "Mason & Hamlin", model: "BB", serial: "lkSFns42", year: 1949, image: "https://www.jimlaabsmusicstore.com/wp-content/uploads/2019/06/Mason-Hamlin-BB.jpeg", notes: "surpisingly musical")
+f = Piano.new(make: "Mason & Hamlin", model: "BB", serial: "lkSFns42", year: 1949, images: "https://www.jimlaabsmusicstore.com/wp-content/uploads/2019/06/Mason-Hamlin-BB.jpeg", notes: "surpisingly musical")
 
-g = Piano.new(make: "Boston", serial: "lkjans42", year: 1993, image: "https://i.ebayimg.com/images/g/kAsAAOSwKtlWq~YX/s-l1600.jpg", notes: "none too shabby!")
+g = Piano.new(make: "Boston", serial: "lkjans42", year: 1993, images: "https://i.ebayimg.com/images/g/kAsAAOSwKtlWq~YX/s-l1600.jpg", notes: "none too shabby!")
 a.pianos << d
 b.pianos << e
 c.pianos << f
